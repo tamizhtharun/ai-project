a5a490901d298ace1f78dc68c7dc88ea7460df0b
tamizhtharun <mailtotharun23@gmail.com>
2024-08-22 12:47:09 +0530

    categories, Card

--- index.js
+++ index.js
@@ -339,3 +339,50 @@ document.addEventListener("DOMContentLoaded", function(){
     // end if innerWidth
     }); 
     // DOMContentLoaded  end
+
+
+ const swiper = new Swiper('.slider-wrapper', {
+      loop: true,
+      grabCursor: true,
+      spaceBetween: 30,
+    
+      //mouse play
+      mousewheel: {
+        invert: false,
+        releaseOnEdges: true,
+        forceToAxis: true
+      },
+
+  // Autoplay
+  // autoplay: {
+  //   delay: 3000, // delay between slides in milliseconds
+  //   disableOnInteraction: false, // disable autoplay when user interacts with the slider
+  // },
+
+      // Pagination
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+      },
+
+         
+      // Navigation
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    
+      // Responsive
+      breakpoints: {
+        0: {
+          slidesPerView: 1
+        },
+        768: {
+          slidesPerView: 2
+        },
+        1024: {
+          slidesPerView: 3
+        }
+      }
+    });
